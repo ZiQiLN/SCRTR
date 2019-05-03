8accff30f4284d689a801ba2c537d948407f3831
Jakub Olan <jakub.olan001@gmail.com>
2019-05-03 13:38:03 +0200

    Wipe of branch

--- src/utilities/endpoints.js
+++
@@ -1,8 +0,0 @@
-// List of all the Endpoints / URLs
-// used in the application
-
-export const URL =  {
-    tradingViewScriptURL: 'https://s3.tradingview.com/tv.js',
-    cryptoCompareURL: 'https://min-api.cryptocompare.com',
-    cryptoStreamURL: 'wss://streamer.cryptocompare.com'
-}
